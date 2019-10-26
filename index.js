ea6249b919871cfce7fff1c4fc68ebfb0a145d80
Gaurav Kulkarni <gaurav@gauravkulkarni.com>
2019-10-26 16:32:05 -0700

    port to env var

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const basicAuth = require('express-basic-auth')
-const port = 3000
+const port = process.env.PORT || 3000
 
 const { getShortlink, setShortlink } = require('./shortener.js')
 
